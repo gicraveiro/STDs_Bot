02c648731c71e6fd602dd5d1b4361348c127046b
gicraveiro <gmcraveiro@hotmail.com>
2022-01-20 21:25:34 +0100

    added sketch database csv for testing, it worked!

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,26 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import csv
+import pandas as pd
+
+class ActionListNames(Action):
+
+    def name(self) -> Text:
+        return "action_list_names"
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            #df = pd.read_csv('FinalProject/data/STDSDatabase.csv')
+            df = pd.read_csv('STDSDatabase.csv')
+
+            for i, j in df.iterrows():
+                dispatcher.utter_message(text=j['Name'])
+            
+            return []
